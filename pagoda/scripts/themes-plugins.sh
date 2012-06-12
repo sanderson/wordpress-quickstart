9f3daf48c498b362b6f75c0c6cdd3a69aa9e5002
Scott Anderson <sanderson@delorum.com>
2012-06-12 17:15:18 -0600

    beginnings of a wordpress quickstart

---
+++ pagoda/scripts/themes-plugins.sh
@@ -0,0 +1,15 @@
+cd wp-content/plugins
+tar -xz < <(curl -s http://wp-qs-assets.pagodabox.com/plugins.tar.gz wp-content/plugins)
+cd plugins
+mv * ..
+cd ../
+rm -rf plugins
+cd ../../
+
+cd wp-content/themes
+tar -xz < <(curl -s http://wp-qs-assets.pagodabox.com/themes.tar.gz wp-content/themes)
+cd /themes
+mv * ..
+cd ../
+rm -rf themes
+cd ../../
